13f7840581332d5e1770b23bdd4dd8c9de155923
ashercn97 <131724380+ashercn97@users.noreply.github.com>
2023-08-09 20:21:31 -0400

    Update mainTraj.py

--- mainTraj.py
+++ mainTraj.py
@@ -49,8 +49,32 @@ def putInClass(arr):
         particleData.append(Particles(arr[i][0], arr[i][1], arr[i][2], arr[i][3], arr[i][4], arr[i][5], arr[i][6]))
     return particleData
 
-output = doData('initial_conditions.txt')
-final = putInClass(output)
+def addAcceleration(particleArr, startingIndex):
+    #starting index is the index that is currently being used, if I was 
+    #evalutating the total a on index 3,startingIndex would = 3
+    length = len(particleArr)
+    startingX = 0
+    startingY = 0
+    startingZ = 0
+    for i in range(length):
+        if i == startingIndex:
+            pass
+        else:
+            currentA = particleArr[startingIndex].acceleration(particleArr[i])
+            Ax = currentA[0]
+            Ay = currentA[1]
+            Az = currentA[2]
+            
+            startingX += Ax
+            startingY += Ay
+            startingZ += Az
+    totalAcceleration = np.array([startingX, startingY, startingZ])
+    return totalAcceleration
+                
+            
+
+#output = doData('initial_conditions.txt')
+#final = putInClass(output)
 
 
 
